4d0a01c7ed3a0ff8f2293592332d4e99a8d0c7c3
mouadziyani <ziyanimouad@gmail.com>
2026-09-07 17:01:37 +0100

    feat add loadingApp function in Console App

---
+++ src/main/java/ma/youcode/lineperm/ui/ConsoleApp.java
@@ -0,0 +1,13 @@
+package ma.youcode.lineperm.ui;
+
+public class ConsoleApp{
+
+    public void LoadApp(){
+        System.out.println("==============================");
+        System.out.println("LinePermission: gestion des fichiers et permissions");
+        System.out.println("==============================");
+        System.out.println("Non connecter. Commandes : signup | login | help | exit");
+        System.out.print("lineperm>");
+    }
+
+}
