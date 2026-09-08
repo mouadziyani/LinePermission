388322434b031bf4c8a84ae2ddb32e740dfdcf67
mouadziyani <ziyanimouad@gmail.com>
2026-09-08 16:46:39 +0100

    refactor: update application entry point

---
+++ src/main/java/ma/youcode/lineperm/Main.java
@@ -0,0 +1,12 @@
+package ma.youcode.lineperm;
+
+import ma.youcode.lineperm.ui.ConsoleApp;
+
+public class Main{
+	
+	public static void main (String[] args){
+		ConsoleApp start = new ConsoleApp();
+		start.LoadApp();
+	}
+
+}
